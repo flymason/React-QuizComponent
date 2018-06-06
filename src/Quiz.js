092dce44f9cbed0ea587f15b1eee307836871383
Mason, Bryan <bryan.mason@aruplab.com>
2018-06-06 12:43:16 -0600

    Module 5: Event handling

--- src/Quiz.js
+++ src/Quiz.js
@@ -21,10 +21,15 @@ class Quiz extends Component
         return (<div>
                 { isQuizEnd ?
                     <QuizEnd /> : 
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                    showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
                 </div>
                 
             );
+    }
+
+    showNextQuestion() {
+        this.setState({quiz_position: this.state.quiz_position + 1});
     }
 }
 
