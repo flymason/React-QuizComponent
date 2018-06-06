9b86d83e0f9ffe9c4e28550fdebd741701edc663
Mason, Bryan <bryan.mason@aruplab.com>
2018-06-06 12:50:10 -0600

    Module 7: Error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,12 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {incorrectAnswer: false};
+    }
+
     render () {
 
         return (
@@ -17,15 +23,20 @@ class QuizQuestion extends Component {
                                 clickHandler={this.handleClick.bind(this)}/>)}
             </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
         </main>
         );
     }
 
     handleClick(button_text)
-    {
+    {        
         if (button_text == this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
+            this.setState({incorrectAnswer:false});
         }
+        else {
+                this.setState({ incorrectAnswer: true });
+            }
     }
 }
 
